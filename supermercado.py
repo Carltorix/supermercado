5e7382f7711d7e66dd05a5fc1f5a12d56cda14e0
Carltorix <carlosacomba@gmail.com>
2020-07-28 19:23:43 -0300

    agregue clase supermercado

--- supermercado.py
+++ supermercado.py
@@ -1,5 +1,5 @@
 class Prodcuto:
-	def __init(self,nombre,precio,primera_necesidad, descuento):
+	def __init__(self,nombre,precio,primera_necesidad, descuento):
 		self.__nombre=nombre
 		self.__precio=precio
 		self.__primera_necesidad=primera_necesidad
@@ -14,5 +14,25 @@ class Prodcuto:
 			return self.__precio*self.__descuento
 		else:
 			return self.__precio
+
+class Supermercado:
+	def __init__(self, nombre,direccion):
+		self.__nombre=nombre
+		self.__direccion=direccion
+		self.__lista_productos=[]
+
+	def get_nombre(self):
+		return self.__nombre	
+					
+	def get_direccion(self):
+		return self.__direccion
+
+	def	set_lista_productos(self,producto):
+		self.__lista_productos.append(producto)
+
+	def cantidad_de_productos(self):
+		return len(set_lista_productos())
+		
+
 
 
