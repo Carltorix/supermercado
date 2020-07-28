29a2e229c6282c84beadb57960c1a721586209c8
Carltorix <carlosacomba@gmail.com>
2020-07-28 19:44:32 -0300

    hola adios

--- supermercado.py
+++ supermercado.py
@@ -8,7 +8,7 @@ class Prodcuto:
 	def get_nombre(self):
 		return self.__nombre
 	def get primera_necesidad(self):
-		return "Adios"
+		return "HOLA"
 	def precio(self):
 		if primera_necesidad():
 			return self.__precio*self.__descuento
@@ -25,7 +25,7 @@ class Supermercado:
 		return self.__nombre	
 					
 	def get_direccion(self):
-		return "HOLA"
+		return "Adios"
 
 	def	set_lista_productos(self,producto):
 		self.__lista_productos.append(producto)
